b5923d1cdc657894fdcc08ce880016b4014b85dc
tech-in-tech <anubhavsharma06dec@gmail.com>
2025-11-06 16:20:37 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { ENV } from './lib/env.js'
 import path from "path";
+import cors from 'cors'
+import {serve} from 'inngest/express'
 import { connectDB } from './lib/db.js';
+import { inngest } from './lib/inngest.js';
 // import cors from "cors";
 
 
@@ -10,9 +13,17 @@ const app = express();
 
 const __dirname = path.resolve()
 
+// moiddlewares 
+app.use(express.json())
+// 
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions}))
+
 app.get('/health', (req, res) => {
   res.status(200).json({ msg: "api is up and running" })
 })
+
 
 app.get('/books', (req, res) => {
   res.status(200).json({ msg: "this is the books endpoints" })

dc9b163fc4dcaf24a5c547be372f67288eb59877
tech-in-tech <anubhavsharma06dec@gmail.com>
2025-12-12 18:26:17 +0530

    session routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { connectDB } from './lib/db.js';
 import { inngest,functions } from './lib/inngest.js';
 // import cors from "cors";
 import chatRoutes from "./routes/chatRoutes.js"
+import sessionRoutes from "./routes/sessionRoute.js"
 
   
 
@@ -25,6 +26,7 @@ app.use(clerkMiddleware()) // this adds auth field to request object : req.auth(
 app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.use("/api/chat",chatRoutes)
+app.use("/api/session",sessionRoutes)
 
 app.get('/health', (req, res) => {
   res.status(200).json({ msg: "api is up and running" })
